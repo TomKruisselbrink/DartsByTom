b511c5d82175414b6f613a3574e16057e8ce5415
TomKruisselbrink <43822008+TomKruisselbrink@users.noreply.github.com>
2026-09-20 14:28:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'darts-v173-20260918';
+const CACHE = 'darts-v174-20260920';
 const FILES = ['/DartsByTom/', '/DartsByTom/index.html', '/DartsByTom/manifest.json'];
 
 self.addEventListener('install', function(e) {
